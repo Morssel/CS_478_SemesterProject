28c5b8793f9e694b44cc23b2efea40e93199f29d
Al <achuangsw@gmail.com>
2019-10-14 05:19:18 -0500

    file upload prototype

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+# import magic
+import urllib.request
+from app import app
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/FLASK_BDDT/')
+def upload_form():
+    return render_template('upload.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            return redirect('/FLASK_BDDT/')
+        else:
+            flash('Allowed file types are txt, pdf')
+            return redirect(request.url)
+
+
+if __name__ == "__main__":
+    app.run()
