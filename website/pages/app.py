be1dfbb9ebe882c28cfd6cab80572b02cadf3727
jcanderson96 <jace.anderson96@gmail.com>
2019-11-14 20:28:59 -0600

    merging parser with website

--- website/pages/app.py
+++ website/pages/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 #UPLOAD_FOLDER = 'C:/wamp64/www/FLASK_BDDT/Uploads'
-UPLOAD_FOLDER = 'C:/Users/jcand/Documents/buildingwebsite/website/pages'
+UPLOAD_FOLDER = 'C:/Users/jcand/Documents/websiteforbddt/website/pages'
 
 app = Flask(__name__)
 app.secret_key = "secret key"
