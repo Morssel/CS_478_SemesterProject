5d5aa5054bbbdc676e839a835ee9c110dc983c5f
jcanderson96 <jace.anderson96@gmail.com>
2019-11-14 20:27:18 -0600

    merging parser with website

---
+++ website/pages/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+#UPLOAD_FOLDER = 'C:/wamp64/www/FLASK_BDDT/Uploads'
+UPLOAD_FOLDER = 'C:/Users/jcand/Documents/buildingwebsite/website/pages'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
